b001d7c509006b6712c9e00dd0fb08cfd7adcf22
David Kasakaitis <davidkasakaitis@proton.me>
2024-05-16 16:23:32 +0100

    feat: workspace api

---
+++ src/modules/workspace.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from .meta import TogglCachedEndpoint
+from .models import TogglWorkspace
+
+
+class CachedWorkspaceEndpoint(TogglCachedEndpoint):
+    def get_workspace(self, *, refresh: bool = False) -> TogglWorkspace:
+        response = self.request("", refresh=refresh)
+        return self.model.from_kwargs(**response)
+
+    @property
+    def model(self) -> type[TogglWorkspace]:
+        return TogglWorkspace
+
+    @property
+    def endpoint(self) -> str:
+        return super().endpoint + f"workspaces/{self.workspace_id}"
+
+    @property
+    def cache_path(self) -> Path:
+        return super().cache_path / "workspace.json"
---
+++ tests/test_workspace.py
@@ -0,0 +1,24 @@
+import pytest
+
+from src.modules.models import TogglWorkspace
+
+
+@pytest.mark.unit()
+def test_workspace_model():
+    data = {
+        "id": 1100,
+        "name": "test",
+    }
+    workspace = TogglWorkspace.from_kwargs(**data)
+    assert isinstance(workspace, TogglWorkspace)
+    assert workspace.id == data["id"]
+    assert workspace.name == data["name"]
+
+
+@pytest.mark.integration()
+def test_get_workspace(workspace_object):
+    data = workspace_object.get_workspace(refresh=True)
+    assert isinstance(data, TogglWorkspace)
+    assert data.id == workspace_object.workspace_id
+    assert workspace_object.cache_path.exists()
+    workspace_object.cache_path.unlink()
