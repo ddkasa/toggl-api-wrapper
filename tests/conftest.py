780ed282ed0b7f6da9a5a65afcdde2ff192d4f4d
David Kasakaitis <davidkasakaitis@proton.me>
2024-05-17 15:29:24 +0100

    test: conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,73 @@ from pathlib import Path
 import pytest
 from httpx import BasicAuth
 
+from src.config import generate_authentication
+from src.modules.client import ClientCachedEndpoint, ClientEndpoint
+from src.modules.project import ProjectCachedEndpoint, ProjectEndpoint
+from src.modules.tag import CachedTagEndpoint, TagEndpoint
+from src.modules.tracker import TrackerCachedEndpoint, TrackerEndpoint
+from src.modules.user import UserCachedEndpoint, UserEndpoint
+from src.modules.workspace import CachedWorkspaceEndpoint
+
 
 @pytest.fixture(scope="session")
-def config_setup() -> tuple[str, str, str]:
-    email = os.getenv("TOGGL_API_TOKEN")
-    password = os.getenv("TOGGL_PASSWORD", "api_token")
-    if email is None:
-        email = os.getenv("TOGGL_EMAIL")
-        if email is None:
-            msg = "Email or API Key not set."
-            raise ValueError(msg)
+def client_object(get_workspace_id, config_setup) -> ClientEndpoint:
+    return ClientEndpoint(get_workspace_id, config_setup)
 
-    return BasicAuth(email, password)
+
+@pytest.fixture(scope="session")
+def cached_client_object(cache_path, get_workspace_id, config_setup) -> ClientCachedEndpoint:
+    return ClientCachedEndpoint(cache_path, get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def tag_object(get_workspace_id, config_setup) -> TagEndpoint:
+    return TagEndpoint(get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def cached_tag_object(cache_path, get_workspace_id, config_setup) -> CachedTagEndpoint:
+    return CachedTagEndpoint(cache_path, get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def project_object(get_workspace_id, config_setup) -> ProjectEndpoint:
+    return ProjectEndpoint(get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def cached_project_object(cache_path, get_workspace_id, config_setup) -> ProjectCachedEndpoint:
+    return ProjectCachedEndpoint(cache_path, get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def workspace_object(cache_path, get_workspace_id, config_setup) -> CachedWorkspaceEndpoint:
+    return CachedWorkspaceEndpoint(cache_path, get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def user_object(config_setup, get_workspace_id) -> UserEndpoint:
+    return UserEndpoint(get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def cached_user_object(cache_path, get_workspace_id, config_setup) -> UserCachedEndpoint:
+    return UserCachedEndpoint(cache_path, get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def tracker_model(get_workspace_id, config_setup) -> TrackerEndpoint:
+    return TrackerEndpoint(get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def cache_tracker_model(get_workspace_id, config_setup) -> TrackerCachedEndpoint:
+    return TrackerCachedEndpoint(Path("files"), get_workspace_id, config_setup)
+
+
+@pytest.fixture(scope="session")
+def config_setup() -> BasicAuth:
+    return generate_authentication()
 
 
 @pytest.fixture(scope="session")
@@ -27,4 +82,4 @@ def cache_path() -> Path:
 
 @pytest.fixture(scope="session")
 def get_workspace_id() -> int:
-    return int(os.getenv("TOGGL_WORKSPACE_ID", ""))
+    return int(os.getenv("TOGGL_WORKSPACE_ID", "0"))
