40930aa1bff4d9f39fdaecff1acd9a14670d47d3
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-04 13:26:28 +0100

    feat(reports): module level imports

---
+++ toggl_api/modules/reports/__init__.py
@@ -0,0 +1,9 @@
+from .reports import DetailedReportEndpoint, ReportBody, ReportEndpoint, SummaryReportEndpoint, WeeklyReportEndpoint
+
+__all__ = (
+    "DetailedReportEndpoint",
+    "ReportBody",
+    "ReportEndpoint",
+    "SummaryReportEndpoint",
+    "WeeklyReportEndpoint",
+)
