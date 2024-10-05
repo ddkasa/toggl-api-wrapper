d2900b999bec71617871f28df844a75edae24bc9
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-05 16:50:05 +0100

    deprecate: modules namespace

--- toggl_api/modules/__init__.py
+++ toggl_api/modules/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from .client import ClientBody, ClientEndpoint
 from .meta import JSONCache, SqliteCache
 from .project import ProjectBody, ProjectEndpoint
@@ -6,6 +8,11 @@ from .tracker import TrackerBody, TrackerEndpoint
 from .user import UserEndpoint
 from .workspace import WorkspaceEndpoint
 
+warnings.warn(
+    "The 'modules' namespace will be merged with the 'toggl_api' namespace in v1.0.0",
+    DeprecationWarning,
+    stacklevel=2,
+)
 __all__ = (
     "ClientBody",
     "ClientEndpoint",
