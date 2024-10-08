8733cedf875fb2bae080e4bd764c28ba11c974a8
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-08 12:24:23 +0100

    feat(utility): requires decorator
    Implemented decorator for checking if optional dependencies are present.

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -2,16 +2,16 @@ from datetime import datetime, timezone
 
 import pytest
 
-from toggl_api.utility import format_iso, get_workspace, parse_iso
+from toggl_api.utility import format_iso, get_workspace, parse_iso, requires
 
 
-@pytest.mark.unit()
+@pytest.mark.unit
 def test_format_iso():
     iso = format_iso(datetime.now(tz=timezone.utc))
     assert isinstance(iso, str)
 
 
-@pytest.mark.unit()
+@pytest.mark.unit
 def test_parse_iso():
     iso = parse_iso("2020-01-01T01:01:01Z")
     assert isinstance(iso, datetime)
@@ -29,7 +29,7 @@ def test_parse_iso():
     )
 
 
-@pytest.mark.unit()
+@pytest.mark.unit
 @pytest.mark.parametrize(
     ("data", "result"),
     [
@@ -40,9 +40,26 @@ def test_parse_iso():
     ],
 )
 def test_get_workspace(data, result):
-    if type(result) == type and issubclass(result, Exception):
+    if type(result) is type and issubclass(result, Exception):
         with pytest.raises(result) as excinfo:
             get_workspace(data)
         assert isinstance(excinfo.value, result)
     else:
         assert get_workspace(data) == result
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "module",
+    [
+        "sqlalchemy",
+        pytest.param("numpy", marks=pytest.mark.xfail(ImportError, reason="NumPy is not a required dependency.")),
+        "httpx",
+    ],
+)
+def test_requires_decorator(module, monkeypatch):
+    @requires(module)
+    def test(a):
+        return a
+
+    assert test(module) == module
