db80c4d9300236a685e530e4bc896070bb2e9dff
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-10 10:50:33 +0100

    chore(nvim): function for generating official docstrings

--- .nvim.lua
+++ .nvim.lua
@@ -3,5 +3,12 @@ table.insert(require("dap").configurations.python, {
 	request = "launch",
 	name = "debug file",
 	program = "${file}",
-	-- ... more options, see https://github.com/microsoft/debugpy/wiki/debug-configuration-settings
 })
+
+function paste_in_official_docs()
+	local pos = vim.api.nvim_win_get_cursor(0)[2]
+	local line = vim.api.nvim_get_current_line()
+	local clipboard_content = vim.fn.getreg("+")
+	local nline = line:sub(0, pos) .. "[Official Documentation](" .. clipboard_content .. ")" .. line:sub(pos + 1)
+	vim.api.nvim_set_current_line(nline)
+end
