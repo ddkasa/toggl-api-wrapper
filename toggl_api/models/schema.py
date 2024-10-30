e72cd7c7767095ef9208c4fd0ce1bb24b5bcc502
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-30 14:52:08 +0000

    feat(schema): added organization model

--- toggl_api/models/schema.py
+++ toggl_api/models/schema.py
@@ -14,7 +14,7 @@ with contextlib.suppress(ImportError):
 
 from toggl_api.utility import requires
 
-from .models import TogglClient, TogglProject, TogglTag, TogglTracker, TogglWorkspace
+from .models import TogglClient, TogglOrganization, TogglProject, TogglTag, TogglTracker, TogglWorkspace
 
 with contextlib.suppress(ImportError):
     from ._decorators import UTCDateTime
@@ -29,6 +29,20 @@ if TYPE_CHECKING:
 def register_tables(engine: Engine) -> MetaData:
     metadata = db.MetaData()
 
+    organization = db.Table(
+        "organization",
+        metadata,
+        db.Column(
+            "created",
+            UTCDateTime(timezone=True),
+            server_default=func.now(),
+        ),
+        db.Column("timestamp", UTCDateTime(timezone=True)),
+        db.Column("id", db.Integer, primary_key=True),
+        db.Column("name", db.String(255)),
+    )
+    _map_imperatively(TogglOrganization, organization)
+
     workspace = db.Table(
         "workspace",
         metadata,
@@ -40,6 +54,7 @@ def register_tables(engine: Engine) -> MetaData:
         db.Column("timestamp", UTCDateTime(timezone=True)),
         db.Column("id", db.Integer, primary_key=True),
         db.Column("name", db.String(255)),
+        db.Column("organization", db.Integer),
     )
 
     _map_imperatively(TogglWorkspace, workspace)
