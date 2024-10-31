ae222dc1374aef32b970808fe59d5bd205f930e5
David Kasakaitis <davidkasakaitis@proton.me>
2024-10-31 13:12:27 +0000

    feat: add organization objects to __init__

--- toggl_api/__init__.py
+++ toggl_api/__init__.py
@@ -2,13 +2,8 @@ from .client import ClientBody, ClientEndpoint
 from .config import generate_authentication
 from .meta import JSONCache
 from .meta.cache import Comparison, TogglQuery
-from .models import (
-    TogglClient,
-    TogglProject,
-    TogglTag,
-    TogglTracker,
-    TogglWorkspace,
-)
+from .models import TogglClient, TogglOrganization, TogglProject, TogglTag, TogglTracker, TogglWorkspace
+from .organization import OrganizationEndpoint
 from .project import ProjectBody, ProjectEndpoint
 from .tag import TagEndpoint
 from .tracker import TrackerBody, TrackerEndpoint
@@ -21,7 +16,7 @@ __version__ = version
 __typed__ = True
 
 
-__all__ = (
+__all__ = (  # noqa: RUF022
     "ClientBody",
     "ClientEndpoint",
     "Comparison",
@@ -42,4 +37,6 @@ __all__ = (
     "WorkspaceBody",
     "WorkspaceEndpoint",
     "generate_authentication",
+    "TogglOrganization",
+    "OrganizationEndpoint",
 )
