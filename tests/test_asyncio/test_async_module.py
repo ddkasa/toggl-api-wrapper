2b93f54860d68cec22df888b0f3d973735051418
David Kasakaitis <davidkasakaitis@proton.me>
2025-01-21 14:44:03 +0000

    refactor(async): update __init__ file

---
+++ tests/test_asyncio/test_async_module.py
@@ -0,0 +1,24 @@
+import importlib
+import sys
+from unittest import mock
+
+import pytest
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "module",
+    [
+        "sqlalchemy",
+        "greenlet",
+    ],
+)
+def test_module_reqs(module):
+    with mock.patch.dict(sys.modules):
+        sys.modules[module] = None
+
+        with pytest.raises(SystemExit):  # noqa: PT012
+            if "toggl_api.asyncio" in sys.modules:
+                importlib.reload(sys.modules["toggl_api.asyncio"])
+            else:
+                importlib.import_module("toggl_api.asyncio")
--- toggl_api/asyncio/__init__.py
+++ toggl_api/asyncio/__init__.py
@@ -1,5 +1,46 @@
-from ._async_project import AsyncProjectEndpoint
-from ._async_sqlite_cache import AsyncSqliteCache, async_register_tables
-from ._async_tracker import AsyncTrackerEndpoint
+from importlib.util import find_spec
 
-__all__ = ("AsyncProjectEndpoint", "AsyncSqliteCache", "AsyncTrackerEndpoint", "async_register_tables")
+if not find_spec("sqlalchemy"):
+    msg = "SQLAlchemy is required for the async module!"
+    raise SystemExit(msg)
+
+if not find_spec("greenlet"):
+    msg = "Greenlet is required for the async module!"
+    raise SystemExit(msg)
+
+
+from ._async_cache import TogglAsyncCache
+from ._async_client import AsyncClientEndpoint
+from ._async_endpoint import TogglAsyncCachedEndpoint, TogglAsyncEndpoint
+from ._async_organization import AsyncOrganizationEndpoint
+from ._async_project import AsyncProjectEndpoint
+from ._async_reports import (
+    AsyncDetailedReportEndpoint,
+    AsyncReportEndpoint,
+    AsyncSummaryReportEndpoint,
+    AsyncWeeklyReportEndpoint,
+)
+from ._async_sqlite_cache import AsyncSqliteCache, async_register_tables
+from ._async_tag import AsyncTagEndpoint
+from ._async_tracker import AsyncTrackerEndpoint
+from ._async_user import AsyncUserEndpoint
+from ._async_workspace import AsyncWorkspaceEndpoint
+
+__all__ = (
+    "AsyncClientEndpoint",
+    "AsyncDetailedReportEndpoint",
+    "AsyncOrganizationEndpoint",
+    "AsyncProjectEndpoint",
+    "AsyncReportEndpoint",
+    "AsyncSqliteCache",
+    "AsyncSummaryReportEndpoint",
+    "AsyncTagEndpoint",
+    "AsyncTrackerEndpoint",
+    "AsyncUserEndpoint",
+    "AsyncWeeklyReportEndpoint",
+    "AsyncWorkspaceEndpoint",
+    "TogglAsyncCache",
+    "TogglAsyncCachedEndpoint",
+    "TogglAsyncEndpoint",
+    "async_register_tables",
+)
