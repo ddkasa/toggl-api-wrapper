ed47e4d4f4bf6b1dd2995ce86dd37a7d75f26423
David Kasakaitis <davidkasakaitis@proton.me>
2025-04-08 11:59:01 +0100

    docs(scripts): update diagram script to new src layout

--- scripts/create_diagrams.sh
+++ scripts/create_diagrams.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Create Mermaid Charts..."
-pyreverse -o mmd toggl_api -d docs/static/mermaid/ --color-palette="#77AADD.#99DDFF"
+pyreverse -o mmd src/toggl_api -d docs/static/mermaid/ --color-palette="#77AADD.#99DDFF"
 
 echo "Creating Project Structure..."
-tree toggl_api --gitignore -I __pycache__ >docs/api-guide/project_structure.txt
+tree src/toggl_api --gitignore -I __pycache__ >docs/api-guide/project_structure.txt
 
 echo "Done!"
